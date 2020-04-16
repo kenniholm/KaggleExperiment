b152dd84ed91e545d7e7665f4a914dd9b62545dd
Kenni Holm <kens123@hotmail.dk>
2020-04-16 13:26:29 +0200

    All data is loading and wrapped correctly
    Just need to sort it and make it display correctly on the linechart

--- app/src/main/java/com/example/aacharttest/MainActivity.kt
+++ app/src/main/java/com/example/aacharttest/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.aacharttest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.opencsv.CSVReader
 import com.opencsv.CSVReaderBuilder
 import java.io.File
@@ -14,12 +18,33 @@ import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
+    val data = arrayListOf<StudentData>()
+    val wrappedData = arrayListOf<Entry>()
+    lateinit var chart : LineChart
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val chart: LineChart = findViewById(R.id.chart)
+        chart = findViewById(R.id.chart)
 
+        getDataFromSheet()
+
+        val dataSet = LineDataSet(wrappedData, "labelTest")
+        dataSet.setColor(Color.RED)
+        val lineData = LineData(dataSet)
+        chart.data = lineData
+        chart.invalidate()
+
+    }
+
+    private fun wrapToEntry(){
+        for (student in data){
+            wrappedData.add(object: Entry(student.writingScore, student.mathScore){})
+        }
+    }
+
+    private fun getDataFromSheet(){
         try {
             val inputStream = assets.open("StudentsPerformance.csv")
             val fileReader = InputStreamReader(inputStream)
@@ -27,12 +52,19 @@ class MainActivity : AppCompatActivity() {
 
             val lines = reader.readAll()
             for (line in lines){
-                println(line[0] + "|" + line[1])
+
+                //println(line[0] + "|" + line[1] + line[2] + line[3] + line[4] + line[5] + line[6] + line[7])
+
+                val singleData = StudentData(line[0], line[1], line[2], line[3], line[4], line[5].toFloat(), line[6].toFloat(), line[7].toFloat())
+                data.add(singleData)
             }
         }
         catch (e: IOException){
             println("Reading CSV Error!")
             e.printStackTrace()
+        }
+        finally {
+            wrapToEntry()
         }
     }
 }
---
+++ app/src/main/java/com/example/aacharttest/StudentData.kt
@@ -0,0 +1,5 @@
+package com.example.aacharttest
+
+data class StudentData(var gender : String, var ethnicity: String, var parentalEducation : String,
+var lunch : String, var testPrep : String, var mathScore : Float, var readingScore : Float,
+var writingScore : Float)
