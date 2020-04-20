2b3d5ad1e28335b349b0bfc251e683db003c15cb
Kenni Holm <kens123@hotmail.dk>
2020-04-20 15:56:49 +0200

    Fix sort method to sort properly.
    Change LineChart to Barchart for proper comparison of data.

--- app/src/main/java/com/example/aacharttest/MainActivity.kt
+++ app/src/main/java/com/example/aacharttest/MainActivity.kt
@@ -3,19 +3,18 @@ package com.example.aacharttest
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.*
 import com.opencsv.CSVReaderBuilder
 import java.io.IOException
 import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
-    val data = arrayListOf<StudentData>()
-    val wrappedData = arrayListOf<Entry>()
-    lateinit var chart : LineChart
+    private val data = arrayListOf<StudentData>()
+    private val wrappedData1 = arrayListOf<BarEntry>()
+    private val wrappedData2 = arrayListOf<BarEntry>()
+    private lateinit var chart : BarChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +24,33 @@ class MainActivity : AppCompatActivity() {
 
         getDataFromSheet()
 
-        val dataSet = LineDataSet(wrappedData, "labelTest")
+        val dataSet = BarDataSet(wrappedData1, "Parents have bachelor")
+        val dataSet2 = BarDataSet(wrappedData2, "Parents don't have bachelor")
         dataSet.setColor(Color.RED)
-        val lineData = LineData(dataSet)
-        chart.data = lineData
+        dataSet2.setColor(Color.GREEN)
+        val barData = BarData(dataSet, dataSet2)
+        chart.data = barData
+        chart.setFitBars(true)
+        chart.axisLeft.axisMinimum = 0f
+        chart.axisLeft.axisMaximum = 80f
         chart.invalidate()
 
     }
+    //Sort and show avg of each groups math score
+    private fun sortToEntry(){
+        val mathScores1 = arrayListOf<Float>()
+        val mathScores2 = arrayListOf<Float>()
+            for (studentData in data){
+                if(studentData.parentalEducation == "bachelor's degree"){
+                    mathScores1.add(studentData.mathScore)
+                }
+                else{
+                    mathScores2.add(studentData.mathScore)
+                }
+            }
+        wrappedData1.add(object: BarEntry(0f, mathScores1.average().toFloat()){})
+        wrappedData2.add(object: BarEntry(2f, mathScores2.average().toFloat()){})
 
-    private fun wrapToEntry(){
-        for (student in data){
-            wrappedData.add(object: Entry(student.writingScore, student.mathScore){})
-        }
     }
 
     private fun getDataFromSheet(){
@@ -47,7 +61,6 @@ class MainActivity : AppCompatActivity() {
 
             val lines = reader.readAll()
             for (line in lines){
-
                 val singleData = StudentData(line[0], line[1], line[2], line[3], line[4], line[5].toFloat(), line[6].toFloat(), line[7].toFloat())
                 data.add(singleData)
             }
@@ -57,7 +70,7 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
         finally {
-            wrapToEntry()
+            sortToEntry()
         }
     }
 }
